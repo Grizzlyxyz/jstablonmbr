7ef8920f972253baedca56947eafd2284a10d734
Grizzly <grizzlyxyz@gmail>
2019-01-16 12:05:39 +0100

    new

---
+++ style.js
@@ -0,0 +1,3 @@
+// Créer un tableau du nom de sommes avec une multitude de valeurs numérique
+// créer un tableau du nom de grossesSommes si la valeur excède 60 les pousser dans ce tableau et les retirer du tableau sommes
+let sommes = [14, 7 , 97, 65 , 247 , 67 , 101 , 4 , 34 ,78 , 1 ,900 , 0, 11 , 32 , 23 , 61];
